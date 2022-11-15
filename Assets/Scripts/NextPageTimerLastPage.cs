7617937a9580358a69a991d17379972cd2b3d21b
heyou32 <77083184+heyou32@users.noreply.github.com>
2022-11-15 19:19:33 +0900

    6P 스킵 버튼 추가 / 2P 고래 움직임 느리게
    아이콘 이미지 변경

--- Assets/Scripts/NextPageTimerLastPage.cs
+++ Assets/Scripts/NextPageTimerLastPage.cs
@@ -3,62 +3,100 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class NextPageTimerLastPage : NextPageTimer
 
+// 1st song : 180
+// 2nd song : 200
+// credit        : 92
+public class NextPageTimerLastPage : NextPageTimer
 {
     public GameObject credit;
     public AudioSource firstSong;
-    public float firstSongTime, secondSongTime;
+    public AudioSource secondSong;
+    public float firstSongTime, secondSongTime, creditTime;
     public GameObject popUpExit;
 
-    Coroutine firstCoroutine;
+    Coroutine coroutine;
     public PageProgressBar progressBar;
     public MainSceneUiManager uiManager;
     public List<GameObject> addedUIs;
     public Button btnCredit;
+    public Button btnSecond;
 
+    private void Start()
+    {
+        secondSong = GetComponent<AudioSource>();
+        btnCredit.onClick.AddListener(SkipToCredit);
+        btnSecond.onClick.AddListener(SkipToSencond);
+    }
     void OnEnable()
     {
-        foreach (GameObject item in addedUIs) {
+        foreach (GameObject item in addedUIs)
+        {
             uiManager.uiObjects.Add(item.GetComponent<Image>());
+           // item.SetActive(true);
         }
-
-        btnCredit.onClick.AddListener(SkipToCredit);
-        firstCoroutine = StartCoroutine(StartTimer(firstSongTime, secondSongTime));
+        coroutine = StartCoroutine(StartTimer(firstSongTime, secondSongTime));
     }
     void OnDisable()
     {
-        btnCredit.onClick.RemoveListener(SkipToCredit);
-        foreach (GameObject item in addedUIs) {
+        foreach (GameObject item in addedUIs)
+        {
             uiManager.uiObjects.Remove(item.GetComponent<Image>());
+            item.SetActive(false);
         }
     }
 
 
     IEnumerator StartTimer(float first, float second)
     {
-        Debug.Log($"{this.firstCoroutine} is started");
+        Debug.Log($"{this.coroutine} is started");
         yield return new WaitForSeconds(first);
-        StartCoroutine(Credit(second));
+
+        StartCoroutine(SecondSongPlay());
+        //StartCoroutine(Credit(second));
+    }
+
+    public void SkipToSencond()
+    {
+        StopCoroutine(this.coroutine);
+        firstSong.Stop();
+        progressBar._time = firstSongTime;
+        coroutine = StartCoroutine(SecondSongPlay());
     }
     public void SkipToCredit()
     {
-        Debug.Log($"stop {this.firstCoroutine}");
-        StopCoroutine(this.firstCoroutine);
+        Debug.Log($"stop {this.coroutine}");
+        StopCoroutine(this.coroutine);
         firstSong.Stop();
-        progressBar._time = firstSongTime;
-        StartCoroutine(Credit(secondSongTime));
+        secondSong.time = secondSongTime - creditTime;
+        secondSong.Play();
+        progressBar._time = firstSongTime + secondSongTime - creditTime;
+        StartCoroutine(Credit(creditTime));
+    }
+    IEnumerator SecondSongPlay()
+    {
+        btnSecond.gameObject.SetActive(false);
+        uiManager.uiObjects.Remove(addedUIs[2].GetComponent<Image>());
+        addedUIs[2].SetActive(false);
+
+        secondSong.Play();
+        yield return new WaitForSeconds(secondSongTime - creditTime);
+        StartCoroutine(Credit(creditTime));
     }
     IEnumerator Credit(float time)
     {
+        btnSecond.gameObject.SetActive(false);
         uiManager.uiObjects.Remove(addedUIs[1].GetComponent<Image>());
         addedUIs[1].SetActive(false);
+        uiManager.uiObjects.Remove(addedUIs[2].GetComponent<Image>());
+        addedUIs[2].SetActive(false);
+
         EndingCredit ed = credit.GetComponent<EndingCredit>();
 
         ed.fadeEffect.FadeOut();
         yield return new WaitForSeconds(2);
         ed.fadeEffect.FadeIn();
-        
+
         credit.SetActive(true);
 
         ed.SetEnableCreditVideo(true);
